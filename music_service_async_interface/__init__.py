466489095f679afe1a0f96628633599b485833b0
JuniorJPDJ <git@juniorjpdj.pl>
2020-08-31 22:03:03 +0200

    TODO

--- music_service_async_interface/__init__.py
+++ music_service_async_interface/__init__.py
@@ -1,1 +1,21 @@
 __version__ = '0.1.0'
+
+# TODO: Cover
+#   abstract Cover.get_url()
+#   abstract async Cover.get_async_file()
+
+# TODO: Track
+#   abstract async Track.get_stream_url
+#   implemented async Track.get_async_file (basing on get_stream_url)
+#   abstract async Track.get_metadata()
+
+# TODO: TrackCollection
+#   abstract gen TrackCollection.tracks()
+
+# TODO: MusicServiceObject
+#   abstract MusicServiceObject::from_url()
+#   abstract MusicServiceObject::parse_urls()
+#   abstract MusicServiceObject.get_url()
+
+# TODO: Specify how to resolve collections of collections of tracks
+#   eg. user containing playlists containing tracks or artist containing albums containing tracks
