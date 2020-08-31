d0f4be27b18d001dc0b38caa603dd7136c86b34b
JuniorJPDJ <git@juniorjpdj.pl>
2020-09-01 01:16:43 +0200

    First version of interface

--- music_service_async_interface/__init__.py
+++ music_service_async_interface/__init__.py
@@ -1,21 +1,110 @@
 __version__ = '0.1.0'
 
-# TODO [#1]: Cover
-#   abstract Cover.get_url()
-#   abstract async Cover.get_async_file()
+from abc import ABC, abstractmethod
+from typing import Optional, Type, AsyncGenerator, Dict
 
-# TODO [#2]: Track
-#   abstract async Track.get_stream_url
-#   implemented async Track.get_async_file (basing on get_stream_url)
-#   abstract async Track.get_metadata()
+import aiohttp
 
-# TODO [#3]: TrackCollection
-#   abstract gen TrackCollection.tracks()
+try:
+    from httpseekablefile import AsyncSeekableHTTPFile
+except ImportError:
+    AsyncSeekableHTTPFile = None
 
-# TODO [#4]: MusicServiceObject
-#   abstract MusicServiceObject::from_url()
-#   abstract MusicServiceObject::parse_urls()
-#   abstract MusicServiceObject.get_url()
 
 # TODO [#5]: Specify how to resolve collections of collections of tracks
 #   eg. user containing playlists containing tracks or artist containing albums containing tracks
+
+
+class InvalidURL(Exception):
+    pass
+
+
+class Session(ABC):
+    sess: aiohttp.ClientSession
+    obj: Type['Object']
+
+    async def object_from_url(self, url: str) -> 'Object':
+        for child_cls in self.obj.__subclasses__():
+            try:
+                return await child_cls.from_url(self, url)
+            except InvalidURL:
+                pass
+
+        # If none objects match url, then the url must be invalid
+        raise InvalidURL
+
+    @abstractmethod
+    def is_valid_url(self, url) -> bool:
+        """
+        Performs basic check if string looks like music service URL
+        """
+        ...
+
+    async def parse_urls(self, long_string: str) -> AsyncGenerator['Object', None]:
+        """
+        Parses `long_string` including music service URLs to corresponding music service objects
+        :param long_string: long string including URLs to music service objects
+        :return: generator with music service objects
+        """
+        for word in long_string.split(' '):
+            if self.is_valid_url(word):
+                try:
+                    yield await self.object_from_url(word)
+                except InvalidURL:
+                    pass
+                break
+
+
+class Object(ABC):
+    sess: Session
+
+    @classmethod
+    @abstractmethod
+    async def from_url(cls, sess: Session, url: str) -> 'Object':
+        raise InvalidURL
+
+    @abstractmethod
+    async def get_url(self) -> str:
+        ...
+
+
+class Cover(ABC):
+    sess: Session
+
+    @abstractmethod
+    def get_url(self) -> str:
+        ...
+
+    if AsyncSeekableHTTPFile is not None:
+        async def get_async_file(self, filename: Optional[str] = None) -> AsyncSeekableHTTPFile:
+            return await AsyncSeekableHTTPFile.create(self.get_url(), filename, self.sess.sess)
+
+
+class Track(Object, ABC):
+    @abstractmethod
+    def title(self) -> str:
+        ...
+
+    @abstractmethod
+    async def get_metadata(self) -> Dict[str, str]:
+        """
+        :return: dict containing tags to be directly written on file when doing metadata tagging
+        """
+        ...
+
+    @abstractmethod
+    async def get_file_url(self, *args, **kwargs) -> str:
+        ...
+
+    async def get_async_file(self, filename: Optional[str] = None, *args, **kwargs) -> AsyncSeekableHTTPFile:
+        return await AsyncSeekableHTTPFile.create(
+            await self.get_file_url(*args, **kwargs),
+            self.title if filename is None else filename,
+            self.sess.sess
+        )
+
+
+class TrackCollection(Object, ABC):
+    @abstractmethod
+    async def tracks(self) -> AsyncGenerator[Track, None]:
+        ...
