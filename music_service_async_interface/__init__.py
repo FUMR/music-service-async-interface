3ef2102980931be8485bf3338109f1af237e4b4b
TODO <todo-actions[bot]@users.noreply.github.com>
2020-08-31 20:04:54 +0000

    Collect TODO comments

--- music_service_async_interface/__init__.py
+++ music_service_async_interface/__init__.py
@@ -1,21 +1,21 @@
 __version__ = '0.1.0'
 
-# TODO: Cover
+# TODO [$5f4d57e6cad27e0007ff2cc7]: Cover
 #   abstract Cover.get_url()
 #   abstract async Cover.get_async_file()
 
-# TODO: Track
+# TODO [$5f4d57e6cad27e0007ff2cc8]: Track
 #   abstract async Track.get_stream_url
 #   implemented async Track.get_async_file (basing on get_stream_url)
 #   abstract async Track.get_metadata()
 
-# TODO: TrackCollection
+# TODO [$5f4d57e6cad27e0007ff2cc9]: TrackCollection
 #   abstract gen TrackCollection.tracks()
 
-# TODO: MusicServiceObject
+# TODO [$5f4d57e6cad27e0007ff2cca]: MusicServiceObject
 #   abstract MusicServiceObject::from_url()
 #   abstract MusicServiceObject::parse_urls()
 #   abstract MusicServiceObject.get_url()
 
-# TODO: Specify how to resolve collections of collections of tracks
+# TODO [$5f4d57e6cad27e0007ff2ccb]: Specify how to resolve collections of collections of tracks
 #   eg. user containing playlists containing tracks or artist containing albums containing tracks
